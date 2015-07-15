4cb39565769ebb70446b0c574989a2642fe1ea03
root <root@raspberrypi.(none)>
2015-07-15 14:35:45 +0000

    imooc

--- schemas/user.js
+++ schemas/user.js
@@ -46,8 +46,19 @@ UserSchema.pre('save', function(next){
   });
 });
 
+UserSchema.methods = {
+  comparePassword: function(_password, cb){
+    bcrypt.compare(_password, this.password, function(err, isMatch){
+      if(err){
+        return cb(err);
+      }
+      cb(null, isMatch);
+    });
+  }
+}
+
 UserSchema.statics = {
-  fetch: function(){
+  fetch: function(cb){
     return this
     .find({})
     .sort('meta.updateAt')
